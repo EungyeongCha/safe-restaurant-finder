990c2049fac674aa17e4f84aa097cc3f48693837
eungyungcha <eungyungcha@gmail.com>
2020-09-13 18:02:36 +0900

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// fetch(
+//     'http://211.237.50.150:7080/openapi/11ae7998f8ebf7af8ecdf1328368115cbec9ce4b163d3dcf512df6b98af95baf/json/Grid_20200713000000000605_1/1/999?RELAX_SI_NM=대구'
+//   )
+//     .then((res) => res.json())
+//     .then((data) => {
+//       console.log(data);
+//     });
